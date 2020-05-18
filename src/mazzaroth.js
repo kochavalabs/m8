3da43ddcc6dd1e68b51eb2965f77165b291df2ad
Kyle Bartush <kbartush@kochava.com>
2020-05-18 14:44:21 -0700

    feat(deploy): add a deploy command

--- src/mazzaroth.js
+++ src/mazzaroth.js
@@ -13,7 +13,6 @@ const defaultChannel = '0'.repeat(64)
 const defaultAddr = 'http://localhost:8081'
 const defaultOwner = '3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29'
 const defaultSender = '0'.repeat(64)
-const defaultVersion = '0.1'
 
 /**
  * Helper function to sleep for a specified number of ms.
@@ -441,26 +440,32 @@ subCmd.action(function (val, options) {
 
 const deployCmd = program.command('deploy <val>')
 const deployCmdDescription = `
-Helper for deploying a contract to a mazzaroth network. Takes a json config file,
-a sample config file can be found at
-https://github.com/kochavalabs/full-contract-example/blob/master/deploy.json
+Helper for deploying a contract to a mazzaroth network. Takes a json config file, a sample config file can be found at https://github.com/kochavalabs/mazzaroth-js/blob/develop/example_deploy.json
 
 Examples:
-  mazzaroth-cli deploy ./deploy.json
+  mazzaroth-cli deploy ./example_deploy.json
 `
 
 deployCmd.description(deployCmdDescription)
-  .option('-h --host <s>',
-    'Web address of the host node default: "http://localhost:8081"')
-deployCmd.action(async function (configPath, options) {
+deployCmd.action(async function (configPath) {
   const config = JSON.parse(fs.readFileSync(configPath))
-  const channel = config['channel-id'] || defaultChannel
-  const version = config['contract-version'] || defaultVersion
-  const owner = config['owner'] || defaultOwner
-  let host = options.host || config['host']
-  host = host || defaultAddr
-
+  const channel = defaultChannel || config['channel-id']
+  const host = defaultAddr || config['node-addr']
   const wasmFile = fs.readFileSync(config['contract'])
+  const action = {
+    channelID: channel,
+    nonce: '1',
+    category: {
+      enum: 2,
+      value: {
+        enum: 1,
+        value: {
+          contract: wasmFile.toString('base64'),
+          version: '0.1'
+        }
+      }
+    }
+  }
 
   const configAction = {
     channelID: channel,
@@ -473,7 +478,7 @@ deployCmd.action(async function (configPath, options) {
           channelID: channel,
           contractHash: '0'.repeat(64),
           version: '',
-          owner: owner,
+          owner: defaultOwner,
           channelName: '',
           admins: []
         }
@@ -481,23 +486,8 @@ deployCmd.action(async function (configPath, options) {
     }
   }
 
-  const action = {
-    channelID: channel,
-    nonce: '1',
-    category: {
-      enum: 2,
-      value: {
-        enum: 1,
-        value: {
-          contract: wasmFile.toString('base64'),
-          version: version
-        }
-      }
-    }
-  }
-
-  const sender = config['sender'] || defaultSender
-  const client = new NodeClient(host, sender)
+  const owner = config['owner'] || defaultSender
+  const client = new NodeClient(host, owner)
   await client.transactionSubmit(configAction)
   await sleep(300)
   await client.transactionSubmit(action)
@@ -511,16 +501,16 @@ deployCmd.action(async function (configPath, options) {
   if (config['xdr-types']) {
     xdrTypes = require(path.resolve(config['xdr-types']))
   }
-  const transactions = config['init-transactions']
-  for (const txName in transactions) {
-    const txSet = config['init-transactions'][txName]
-    for (const txIndex in txSet) {
-      const tx = txSet[txIndex]
-      const sender = tx['sender'] || defaultSender
+  const testSets = config['test-sets']
+  for (const setName in testSets) {
+    const testSet = config['test-sets'][setName]
+    for (const testIndex in testSet) {
+      const test = testSet[testIndex]
+      const sender = test['sender'] || defaultSender
       const client = new NodeClient(host, sender)
       const contractClient = new ContractClient(abi, client, xdrTypes, channel)
-      const functionName = tx['function_name']
-      const result = await contractClient[functionName](...tx['args'].map(x => {
+      const functionName = test['function_name']
+      const result = await contractClient[functionName](...test['args'].map(x => {
         if (typeof x === 'object' && x !== null) {
           return JSON.stringify(x)
         }
@@ -534,23 +524,6 @@ deployCmd.action(async function (configPath, options) {
 
 program.on('command:*', function (command) {
   program.help()
-})
-
-const subCmd = program.command('subscribe [val]')
-const subCmdDescription = `
-Subscribes to the receipts received by a readonly/standalone node.
-
-Examples:
-  mazzaroth-cli subscribe '{"receiptFilter": {}, "transactionFilter": {"configFilter":{}}}'
-`
-subCmd.description(subCmdDescription).option('-h --host <s>', 'Web address of the host node default: "localhost:8081"')
-subCmd.action(function (val, options) {
-  options.host = options.host || 'localhost:8081'
-  val = val || '{}'
-  ReceiptSubscribe(options.host, JSON.parse(val), (result) => { console.log(result) })
-  process.stdin.setRawMode(true)
-  process.stdin.resume()
-  process.stdin.on('data', process.exit.bind(process, 0))
 })
 
 program.parse(process.argv)
